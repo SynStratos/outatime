59de9d7292f589a492d0d6c02ae619ac48fc3760
SynStratos <synstratos.dev@gmail.com>
2022-05-11 12:20:57 +0200

    :white_check_mark: Adds tests with custom and missing granularity

--- test/test_inference.py
+++ test/test_inference.py
@@ -1,5 +1,6 @@
 from outatime.dataclass.time_series_data import TimeSeriesData
 from outatime.granularity.granularity import *
+from outatime.timeseries.time_series import TimeSeries
 from outatime.util.relativedelta import relativedelta
 from test.utils import data_generation
 
@@ -21,7 +22,7 @@ def test_granularity_inference():
     assert isinstance(tsl.data_granularity, YearlyGranularity), "Expected YearlyGranularity."
 
 
-def test_granularity_after_insert():
+def test_granularity_inference_after_insert():
     tsl = data_generation(start_date='2020-01-01', end_date='2021-01-01', step=relativedelta(months=1))
     assert isinstance(tsl.data_granularity, MonthlyGranularity), "Expected MonthlyGranularity."
 
@@ -35,7 +36,7 @@ def test_granularity_after_insert():
     assert isinstance(tsl.data_granularity, DailyGranularity), "Expected DailyGranularity after insertion."
 
 
-def test_granularity_after_resample():
+def test_granularity_inference_after_resample():
     tsl = data_generation(start_date='2020-01-01', end_date='2021-01-01', step=relativedelta(days=1))
     assert isinstance(tsl.data_granularity, DailyGranularity), "Expected DailyGranularity."
 
@@ -43,9 +44,68 @@ def test_granularity_after_resample():
     assert isinstance(tsl.data_granularity, MonthlyGranularity), "Expected MonthlyGranularity after insertion."
 
 
-def test_granularity_after_resample_reverse():
+def test_granularity_inference_after_resample_reverse():
     tsl = data_generation(start_date='2020-01-01', end_date='2021-01-01', step=relativedelta(months=1))
     assert isinstance(tsl.data_granularity, MonthlyGranularity), "Expected MonthlyGranularity."
 
     tsl.resample(granularity=DailyGranularity(), inplace=True)
     assert isinstance(tsl.data_granularity, DailyGranularity), "Expected DailyGranularity after insertion."
+
+
+def test_granularity_inference_irregular_steps():
+    tsl = TimeSeries(
+        [
+            TimeSeriesData(day=datetime.strptime('2020-01-31', "%Y-%m-%d").date(), data={}),
+            TimeSeriesData(day=datetime.strptime('2020-02-01', "%Y-%m-%d").date(), data={}),
+            TimeSeriesData(day=datetime.strptime('2020-03-15', "%Y-%m-%d").date(), data={}),
+            TimeSeriesData(day=datetime.strptime('2020-04-01', "%Y-%m-%d").date(), data={}),
+        ]
+    )
+    assert isinstance(tsl.data_granularity, MonthlyGranularity), "Expected MonthlyGranularity."
+
+
+def test_granularity_inference_custom_granularity():
+    class TwoYearGranularity(Granularity):
+        delta = relativedelta(years=2)
+
+        def get_beginning_of_granularity(self, day: date) -> date:
+            year = day.year
+            year -= year % 4
+            day.replace(year=year)
+            return day
+
+        def get_end_of_granularity(self, day: date) -> date:
+            year = day.year + 4
+            year -= year % 4
+            day.replace(year=year)
+            return day
+
+        def assert_included_day(self, day: date, days: int):
+            assert -1 <= days < 365*4 + 1, assertion_err_msg
+
+    granularity_set = [
+        TwoYearGranularity,
+        YearlyGranularity,
+        QuarterlyGranularity,
+        MonthlyGranularity,
+        WeeklyGranularity,
+        DailyGranularity
+    ]
+    tsl = data_generation(
+        start_date='2010-01-01',
+        end_date='2025-01-31',
+        step=relativedelta(years=2),
+        possible_granularity_list=granularity_set
+    )
+    assert isinstance(tsl.data_granularity, TwoYearGranularity), "Expected custom TwoYearGranularity."
+
+
+def test_granularity_inference_missing_granularity():
+    granularity_set = [YearlyGranularity, QuarterlyGranularity, WeeklyGranularity, DailyGranularity]
+    tsl = data_generation(
+        start_date='2010-01-01',
+        end_date='2025-01-31',
+        step=relativedelta(months=1),
+        possible_granularity_list=granularity_set
+    )
+    assert isinstance(tsl.data_granularity, WeeklyGranularity), "Expected WeeklyGranularity because missing MonthlyGranularity."
